aadc443aa3578b64c167b4a7fbe1c1fe7e1baa3a
Emman Nipal <emnnipal@gmail.com>
2023-01-27 11:59:48 +0800

    fix: fix selected timezone not working as intended

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import Clock from '$lib/components/clock.svelte';
   import DateTimePicker from '$lib/components/date-time-picker.svelte';
   import SelectTimezone from '$lib/components/select-timezone.svelte';
+  import { DATE_TIME_FORMAT } from '$lib/constants/date-format';
   import { LocalStorageKey } from '$lib/enums/local-storage';
   import TrashCanRegular from '$lib/icons/trash-can-regular.svelte';
   import autoAnimate from '$lib/utils/auto-animate';
@@ -9,9 +10,13 @@
   import dayjs from 'dayjs';
   import uniq from 'lodash/uniq';
 
-  let dateTimeValue: string = dayjs().format('YYYY-MM-DDTHH:mm');
+  let dateTimeValue: string = dayjs().format(DATE_TIME_FORMAT);
   let selectedTimezone: string = LocalStorageUtil.get(LocalStorageKey.AppTimezone) || 'Asia/Manila';
   let recentTimezones: string[] = LocalStorageUtil.get(LocalStorageKey.RecentTimezones) || [];
+
+  $: derivedDateTimeWithTimezone = dayjs(dateTimeValue)
+    .tz(selectedTimezone)
+    .format(DATE_TIME_FORMAT);
 
   $: LocalStorageUtil.set(LocalStorageKey.RecentTimezones, recentTimezones);
   $: LocalStorageUtil.set(LocalStorageKey.AppTimezone, selectedTimezone);
@@ -22,7 +27,7 @@
     <DateTimePicker label="Set time" bind:value={dateTimeValue} />
   </div>
   <div class="grid grid-cols-[auto_1fr] items-center gap-2 font-semibold">
-    <span> Current Timezone: </span>
+    <span> Set timezone: </span>
     <SelectTimezone bind:selectedTimezone />
   </div>
   <div class="flex flex-col gap-3 border border-gray-100 p-4">
@@ -45,7 +50,11 @@
             </span>
             <span>
               <!-- TODO: check if interval needs to be changed -->
-              <Clock {timezone} dateTime={dateTimeValue} format={'MMM DD - hh:mm a'} />
+              <Clock
+                {timezone}
+                dateTime={derivedDateTimeWithTimezone}
+                format={'MMM DD - hh:mm a'}
+              />
             </span>
           </div>
           <button
