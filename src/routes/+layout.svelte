06481e02cabb1853fe7eb7949caa9b89b51d56e5
Emman Nipal <emnnipal@gmail.com>
2023-07-21 10:33:35 +0800

    feat: add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
   import utc from 'dayjs/plugin/utc';
   import timezone from 'dayjs/plugin/timezone';
   import AppLogo from '$lib/icons/app-logo.svelte';
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+
+  inject({ mode: dev ? 'development' : 'production' });
 
   dayjs.extend(utc);
   dayjs.extend(timezone);
